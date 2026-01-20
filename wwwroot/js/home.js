bbff21e4f003039fc16b1af709a752ddb3c638e8
Dhruv Parmar <dhruvparmar1323@gmail.com>
2026-01-20 11:57:42 +0530

    Add files via upload

---
+++ wwwroot/js/home.js
@@ -0,0 +1,15 @@
+﻿// HOME PAGE JavaScript CODE
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-role", {
+        strings: [
+            "ASP.NET Core Developer",
+            "MVC Developer",
+            "Backend Engineer"
+        ],
+        typeSpeed: 60,
+        backSpeed: 40,
+        backDelay: 1500,
+        loop: true
+    });
+});
